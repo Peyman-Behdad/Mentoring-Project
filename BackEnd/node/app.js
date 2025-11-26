b666fc758735c4cdda69c60c1a8ee43096d8d338
Peyman Behdad <peyman2007behdad@gmail.com>
2025-11-26 13:56:52 -0800

    Adding exercise codes

--- BackEnd/node/app.js
+++ BackEnd/node/app.js
@@ -1,1 +1,28 @@
-console.log("payman");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const courses = [
+  { id: 1, name: "html" },
+  { id: 2, name: "css" },
+  { id: 3, name: "js" },
+];
+
+app.get("/", (req, res) => {
+  res.send("welcome to the server");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(["html", "css", "js"]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("course is not found");
+  res.send(course);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is start on port ${port}`);
+});
