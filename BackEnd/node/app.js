9fba64d9dc85c6c52009ff9120d6e69c58de9fb1
Peyman Behdad <peyman2007behdad@gmail.com>
2025-11-27 14:24:35 -0800

    add crud codes

--- BackEnd/node/app.js
+++ BackEnd/node/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "html" },
@@ -13,13 +14,48 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send(["html", "css", "js"]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) return res.status(404).send("course is not found");
   res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send("name is required");
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("course in not found");
+
+  if (!req.body.name || req.body.name.length < 3) {
+    return res.status(400).send("name is required and more than 3 charachter");
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("course is not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.status(200).send("course deleted");
 });
 
 const port = process.env.PORT || 3001;
